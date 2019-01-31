73dc3d81d9d766b6930d1fa2610fe2dc9d4cd633
Siegfried Maier <siegfried.maier@cohu.com>
2019-01-31 14:03:05 +0100

    Added for Exercises

---
+++ 5_BadDesign/BadDesign/BadDesign/FTP_Bad.cs
@@ -0,0 +1,581 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace BadDesign
+{
+    public class FTP_Bad
+    {
+        //private Logger log = Logger.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private Object lockObject = new Object();
+
+        protected string uri;
+        protected NetworkCredential networkCredential;
+
+        public FTP_Bad(string ipAddress, string user, string password)
+        {
+            this.uri = "ftp://" + ipAddress;
+            this.networkCredential = new NetworkCredential(user, password);
+        }
+
+        public bool IsConnected 
+        {
+            get 
+            {
+                string workingDirectory;
+                return TryGetWorkingDirectory(out workingDirectory); 
+            } 
+        }
+
+        public bool TryGetWorkingDirectory(out string workingDirectory)
+        {
+            workingDirectory = String.Empty;
+            try
+            {
+                string tmpWorkingDirectory = GetWorkingDirectory();
+                if (tmpWorkingDirectory != null)
+                {
+                    workingDirectory = tmpWorkingDirectory;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //log.Exception(ex);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// returns the list of files contained in the given path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>null if failure</returns>
+        private IList<string> GetFilelist(string path, bool detailed)
+        {
+            List<string> files = new List<string>();
+
+            FtpWebResponse ftpResponse = null;
+            StreamReader  serverStream = null;
+         
+            path = EditPath(path);
+
+            lock (this.lockObject)
+            {
+                try
+                {
+                    string requestMethod = detailed ? WebRequestMethods.Ftp.ListDirectoryDetails : WebRequestMethods.Ftp.ListDirectory;
+
+                    FtpWebRequest ftpRequest = GetFtpWebRequestObject(path, requestMethod, true);
+                    ftpRequest.Timeout          = 10000;// 10 * 1000  Timeout does NOT work!
+                    ftpRequest.ReadWriteTimeout = 10000;// 10 * 1000  Timeout does NOT work!
+
+                    ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+
+                    if (ftpResponse != null && ftpResponse.StatusCode == FtpStatusCode.OpeningData)
+                    {
+                        serverStream = new StreamReader(ftpResponse.GetResponseStream());
+
+                        string line = serverStream.ReadLine();
+
+                        while (line != null)
+                        {
+                            line = line.Trim();
+
+                            if (line != "." && line != "..")
+                            {
+                                files.Add(path + line);
+                            }
+
+                            line = serverStream.ReadLine();
+                        }                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //log.Exception(ex);
+                    files = null;
+                }
+                finally
+                {
+                    if (serverStream != null)
+                    {
+                        serverStream.Close();
+                    }
+
+                    if (ftpResponse != null)
+                    {
+                        ftpResponse.Close();
+                    }
+                }
+            }
+            
+            return files;
+        }
+
+        public bool TryGetFilelist(string path, out IList<string> files, bool detailed)
+        {           
+            int retryCounter = 0;
+
+            // since FTP Request without connected IPC lasts very long
+            // TryGetFileList might have a duration of 120 seconds till it fails
+            // may be a ping in the beginning of GetFileList 
+            // could help to reduce time till failure 
+            while (((files = GetFilelist(path, detailed)) == null) && retryCounter < 5)
+            {
+                System.Threading.Thread.Sleep(150);
+                retryCounter++;
+            }
+
+            if (files == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool FileExists(string path)
+        {
+            string folder = path.Contains("/") ? path.Substring(0, path.LastIndexOf("/")) : "";
+
+            IList<string> files = GetFilelist(folder, false);
+
+            if (files == null)
+            {
+                return false;
+            }
+
+            foreach (string filename in files)
+            {
+                if (filename == path)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Ladet eine Datei vom Server
+        /// </summary>
+        /// <param name="sourceFile">Absoluter Pfad am Server Bsp: /stripmap/out/sm.map</param>
+        /// <param name="destinationFile">Lokales Ziel</param>
+        protected Stream DownloadFile(string sourceFile, bool useBinary)
+        {
+            int bufferLength = 2048;
+            int readLength   = 0;
+            byte[] buffer    = new byte[bufferLength];
+
+            FtpWebResponse ftpWebResponse = null;
+            Stream serverStream = null;
+            Stream outputStream = new MemoryStream();
+
+            try
+            {
+                FtpWebRequest ftpRequest = GetFtpWebRequestObject(sourceFile, WebRequestMethods.Ftp.DownloadFile, useBinary);
+                ftpRequest.ReadWriteTimeout = 30000; // 30 * 1000 ms = 30 s
+
+                ftpWebResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                if (ftpWebResponse != null && ftpWebResponse.StatusCode == FtpStatusCode.OpeningData)
+                {
+                    serverStream = ftpWebResponse.GetResponseStream();
+                }
+
+                if (serverStream != null)
+                {
+                    readLength = serverStream.Read(buffer, 0, bufferLength);
+                }
+
+                while (readLength > 0)
+                {
+                    outputStream.Write(buffer, 0, readLength);
+                    readLength = serverStream.Read(buffer, 0, bufferLength);
+                }
+            }
+            catch(Exception ex)
+            {
+                //log.Exception(ex);
+                return null;
+            }
+            finally
+            {
+                if (serverStream != null)
+                {
+                    serverStream.Close();
+                }
+
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+            return outputStream;
+        }
+        
+        /// <summary>
+        /// retrieves a file from server
+        /// </summary>
+        /// <param name="sourceFile">absolute path at server, e.g. /stripmap/out/sm.map</param>
+        /// <param name="outputStream">destination</param>
+        public bool TryDownloadFile(string sourceFile, Stream outputStream, bool useBinary)
+        {
+            try
+            {
+                Stream downloadStream = DownloadFile(sourceFile, useBinary);
+                if (downloadStream == null)
+                    return false;
+
+                downloadStream.Seek(0, SeekOrigin.Begin);
+
+                // copy content of downloaded stream to output stream
+                int length = 2048;
+                Byte[] buffer = new Byte[length];
+                int bytesRead = downloadStream.Read(buffer, 0, length);
+                while (bytesRead > 0)
+                {
+                    outputStream.Write(buffer, 0, bytesRead);
+                    bytesRead = downloadStream.Read(buffer, 0, length);
+                }
+
+                downloadStream.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //log.Exception(ex);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Ladet eine lokale Datei zum Server
+        /// </summary>
+        /// <param name="sourceStream">Stream mit Daten</param>
+        /// <param name="destinationFile">Name und Pfad der Datei auf dem Server. Der Pfad muss immer absolut sein! Bsp: /stripmap/out/sm.map</param>
+        private bool UploadFile(Stream sourceStream, string destinationFile, bool useBinary)
+        {
+            int bufferLength = 2048;
+            byte[] buffer = new byte[bufferLength];
+            int readLength;
+            Stream streamToServer = null;
+
+            try
+            {
+                FtpWebRequest ftpRequest = GetFtpWebRequestObject(destinationFile, WebRequestMethods.Ftp.UploadFile, useBinary);
+                ftpRequest.ReadWriteTimeout = 30000; // 30 * 1000 ms = 30 s
+                ftpRequest.ContentLength = sourceStream.Length;
+
+                streamToServer = ftpRequest.GetRequestStream();
+
+                readLength = sourceStream.Read(buffer, 0, bufferLength);
+
+                while (readLength != 0)
+                {
+                    streamToServer.Write(buffer, 0, readLength);
+                    readLength = sourceStream.Read(buffer, 0, bufferLength);
+                }
+            }
+            catch(Exception ex)
+            {
+                if (ex is InvalidOperationException || ex is WebException || ex is ProtocolViolationException)
+                {
+                    //log.Exception(ex);
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            finally
+            {
+                if (streamToServer != null)
+                {
+                    streamToServer.Close();
+                }
+            }
+            return true;
+        }
+
+        public bool TryUploadFile(Stream sourceStream, string destinationFile, bool useBinary)
+        {
+            try
+            {
+                return UploadFile(sourceStream, destinationFile, useBinary);
+            }
+            catch (Exception ex)
+            {
+                //log.Exception(ex);
+            }
+            return false;
+        }
+
+        protected bool DeleteFile(string destinationFile)
+        {
+            bool returnValue;
+           
+            FtpWebResponse ftpResponse = null;
+            
+            try
+            {
+                FtpWebRequest ftpRequest = GetFtpWebRequestObject(destinationFile, WebRequestMethods.Ftp.DeleteFile, true);
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+
+                if (ftpResponse != null && ftpResponse.StatusCode == FtpStatusCode.FileActionOK)
+                {
+                    returnValue = (ftpResponse.StatusCode == FtpStatusCode.FileActionOK);
+                    
+                    if (!returnValue)
+                    {
+                        throw new Exception("FTP Delete File of file: "+ destinationFile+ " failed!\n" + 
+                                            "Reason: "+ ftpResponse.StatusDescription);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                if (ex is InvalidOperationException || ex is WebException)
+                {
+                    //log.Exception(ex);
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            finally
+            {
+                if (ftpResponse != null)
+                {
+                    ftpResponse.Close();
+                }
+            }
+            return true;
+        }
+
+        public bool TryDeleteFile(string destinationFile)
+        {
+            try
+            {
+                return DeleteFile(destinationFile);
+            }
+            catch (Exception ex)
+            {
+                //log.Exception(ex);
+            }
+            return false;
+        }
+
+        protected FtpWebRequest GetFtpWebRequestObject(string path, string requestMethod, bool useBinary)
+        {            
+            FtpWebRequest ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(this.uri + path);
+
+            ftpWebRequest.KeepAlive = false;            
+            ftpWebRequest.UseBinary = useBinary;
+            ftpWebRequest.Method    = requestMethod;
+
+            //Der FtpWebRequest wird eine Gruppe zugewisen, sodass die selbe Verbindung verwendet wird
+            ftpWebRequest.ConnectionGroupName = "ServiceTool";
+
+            //Die Verbindungen werden auf 2 beschränkt
+            ftpWebRequest.ServicePoint.ConnectionLimit = 2;
+
+            return ftpWebRequest;
+        }
+
+        private static string EditPath(string path)
+        {
+            path = path.Trim();
+
+            if (!path.EndsWith("/"))
+            {
+                path += "/";
+            }
+            return path;
+        }
+
+        private void GetFileFromFtp(string sourceFile, Stream outputStream, bool useBinary)
+        {
+            int bufferLength = 2048;
+            int readLength   = 0;
+            byte[] buffer    = new byte[bufferLength];
+
+            FtpWebResponse ftpWebResponse = null;
+            Stream serverStream = null;
+
+            try
+            {
+                FtpWebRequest ftpRequest = GetFtpWebRequestObject(sourceFile, WebRequestMethods.Ftp.DownloadFile, useBinary);
+
+                ftpWebResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                if (ftpWebResponse != null && ftpWebResponse.StatusCode == FtpStatusCode.OpeningData)
+                {
+                    serverStream = ftpWebResponse.GetResponseStream();
+                }
+
+                if (serverStream != null)
+                {
+                    readLength = serverStream.Read(buffer, 0, bufferLength);
+                }
+
+                while (readLength > 0)
+                {
+                    outputStream.Write(buffer, 0, readLength);
+                    readLength = serverStream.Read(buffer, 0, bufferLength);
+                }
+            }
+            catch(Exception ex)
+            {
+                //log.Exception(ex);
+            }
+            finally
+            {
+                if (serverStream != null)
+                {
+                    serverStream.Close();
+                }
+
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+        }
+
+        private string GetWorkingDirectory()
+        {
+            FtpWebResponse ftpWebResponse = null;
+
+            try
+            {
+                FtpWebRequest ftpRequest = GetFtpWebRequestObject(string.Empty, WebRequestMethods.Ftp.PrintWorkingDirectory, true);
+                ftpRequest.Timeout = 5 * 1000; // 5 s
+
+                ftpWebResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                
+                if (ftpWebResponse != null && ftpWebResponse.StatusCode == FtpStatusCode.PathnameCreated)
+                {
+                    return ftpWebResponse.ResponseUri.AbsolutePath;
+                }
+                else
+                {
+                    string logInfo = string.Format("GetWorkingDirectory Result Status: {0} StatusCode: {1}",
+                                                   ftpWebResponse != null,
+                                                   ftpWebResponse != null ? ftpWebResponse.StatusCode.ToString() : string.Empty);
+                    throw new WebException(logInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is InvalidOperationException || ex is WebException)
+                {
+                    //log.Exception(ex);
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            finally
+            {
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+        }
+
+        private bool DirectoryExistsViaFtpRequest(string path)
+        {
+            FtpWebResponse ftpResponse = null;
+
+            try
+            {
+                FtpWebRequest ftpWebRequest = GetFtpWebRequestObject(path, WebRequestMethods.Ftp.ListDirectory, true);
+                ftpWebRequest.Timeout          = 5 * 1000; // 5 seconds
+                ftpWebRequest.ReadWriteTimeout = 5 * 1000; // 5 seconds
+
+                if (ftpWebRequest == null)
+                {
+                    //log.Error("DirectoryExistsViaFtpRequest ftpWebRequest is null");
+                    return false;
+                }
+
+                ftpResponse = (FtpWebResponse)ftpWebRequest.GetResponse();
+                bool resultStatus = ftpResponse != null && ftpResponse.StatusCode == FtpStatusCode.OpeningData;
+                return resultStatus;
+            }
+            catch (Exception ex)
+            {
+                //log.Exception(ex);
+                if (ex is InvalidOperationException || ex is WebException)
+                {
+                    return false;
+                }
+                else
+                {
+                    // TODO soll die wirklich weitergereicht werden?
+                    throw;
+                }
+            }
+            finally
+            {   
+                if (ftpResponse != null)
+                {
+                    ftpResponse.Close();
+                }
+            }
+        }
+
+        public bool FileExistsViaFtpRequest(string path)
+        {
+            //Pfad muss mit / enden
+            if (!path.EndsWith("/"))
+                path += "/";
+
+            FtpWebRequest ftpWebRequest = GetFtpWebRequestObject(path, WebRequestMethods.Ftp.ListDirectory, true);
+                ftpWebRequest.Timeout          = 5 * 1000; // 5 seconds
+                ftpWebRequest.ReadWriteTimeout = 5 * 1000; // 5 seconds
+
+            //Der FtpWebRequest wird eine Gruppe zugewisen, sodass die selbe Verbindung verwendet wird
+            ftpWebRequest.ConnectionGroupName = "Test";
+
+            //Die Verbindungen werden auf 2 beschränkt
+            ftpWebRequest.ServicePoint.ConnectionLimit = 2;
+
+            ftpWebRequest.KeepAlive = false;            
+            ftpWebRequest.UseBinary = true;
+            ftpWebRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            try
+            {
+                // es wird eine LIst Files Abfrage gemacht, die auch die FileDetails mitanzeigt
+                // denn die funktioniert auch, wenn das Verzeichnis leer ist
+                // wirft aber eine Exception, wenn das Verzeichnis NICHT existiert
+                WebResponse ftpResponse = ftpWebRequest.GetResponse();
+                ftpResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                // Hier die exceptions noch genauer prüfen
+                //MessageBox.Show(ex.Message + (ex.InnerException == null ? "" : ("\n" + ex.InnerException.InnerException)));
+                //MessageBox.Show("File not found: " + path);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 5_BadDesign/BadDesign/BadDesign/IpcTools_Bad.cs
@@ -0,0 +1,194 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BadDesign
+{
+    class IpcTools
+    {
+        public class FtpFileInfo
+        {
+            public enum FtpFileTypes
+            {
+                Unknown,
+                Directory,
+                File
+            };
+            public String Line;
+            public String FullPath { get { return Path + (Path.EndsWith("/") ? "" : "/") + Name; } }
+            public String Path;
+            public String Name;
+            public long   Size;
+            public DateTime date;
+            public int Level;
+            public String Rights;
+            public String Owner;
+            public String Group;
+            public FtpFileTypes Type;
+        };
+
+        public Action<string> OutputCallback = null;
+
+        private FTP_Bad FTPAccess;
+
+        private String Uri { get; set; }
+        private NetworkCredential NetworkCredential { get; set; }
+
+
+
+        public IpcTools(string ipAddress, string user, string password)
+        {
+            FTPAccess = new FTP_Bad(ipAddress, user, password);
+        }
+
+        private void PrintText(string msg)
+        {
+            if (OutputCallback == null)
+                return;
+
+            OutputCallback(msg);
+            System.Diagnostics.Trace.TraceInformation(msg);
+        }
+
+        private int GetMonthFromString(string month)
+        {
+            month = month.ToLower();
+            if      (month.StartsWith("jan")) return  1;
+            else if (month.StartsWith("feb")) return  2;
+            else if (month.StartsWith("mar")) return  3;
+            else if (month.StartsWith("apr")) return  4;
+            else if (month.StartsWith("may")) return  5;
+            else if (month.StartsWith("jun")) return  6;
+            else if (month.StartsWith("jul")) return  7;
+            else if (month.StartsWith("aug")) return  8;
+            else if (month.StartsWith("sep")) return  9;
+            else if (month.StartsWith("oct")) return 10;
+            else if (month.StartsWith("nov")) return 11;
+            else if (month.StartsWith("dec")) return 12;
+            else return 1;
+        }
+
+        public void GetAllFilesRecursive(string path, List<FtpFileInfo> globalFileList, int level)
+        {
+            //PrintText("Processing " + path);
+            List<string> dirList = new List<string>();
+
+            IList<String> detailedFileListStrings = GetDetailedFileNameList(path);
+            List<FtpFileInfo> localFileList = new List<FtpFileInfo>();
+
+            for (int ii = 0; ii < detailedFileListStrings.Count; ii++)
+            {
+                String detailedFileString = detailedFileListStrings[ii];
+
+                // remove the /path/
+                detailedFileString = detailedFileString.Substring(path.Length);
+                if (detailedFileString[0] == '/')
+                    detailedFileString = detailedFileString.Remove(0, 1);
+
+                // eliminate all '  ' double blanks
+                while (detailedFileString.IndexOf("  ") >= 0)
+                    detailedFileString = detailedFileString.Replace("  ", " ");
+
+                string[] items = detailedFileString.Split(' ');
+
+                String fileName = items[items.Length-1];
+
+                if (String.IsNullOrEmpty(fileName) || fileName == "." || fileName == "..")
+                    continue;
+
+                FtpFileInfo ftpFile = new FtpFileInfo();
+                ftpFile.Line   = detailedFileString;
+                ftpFile.Level  = level;
+                ftpFile.Path   = path;
+                ftpFile.Name   = fileName;
+                ftpFile.Type   = items[0][0] == 'd' ? FtpFileInfo.FtpFileTypes.Directory : FtpFileInfo.FtpFileTypes.File;
+                ftpFile.Rights = items[0].Remove(0, 1);
+                ftpFile.Owner  = items[2];
+                ftpFile.Group  = items[3];
+                ftpFile.Size   = Convert.ToInt64(items[4]);
+                ftpFile.date   = new DateTime(Convert.ToInt32(items[7]), GetMonthFromString(items[5]), Convert.ToInt32(items[6])); 
+
+                localFileList.Add(ftpFile);
+
+                PrintText(String.Format("{0, 10}  {1}", ftpFile.Size, ftpFile.FullPath));
+            }
+
+            level++;
+
+            foreach(FtpFileInfo ftpFile in localFileList)
+            {
+                if (ftpFile.Type == FtpFileInfo.FtpFileTypes.Directory)
+                {
+                    GetAllFilesRecursive(ftpFile.FullPath, globalFileList, level);
+                }
+
+                globalFileList.Add(ftpFile);
+            }
+        }
+
+        public IList<String> GetDetailedFileNameList(string path)
+        {
+            IList<string> files = new List<string>();
+
+            if (!FTPAccess.FileExistsViaFtpRequest(path))
+                return files;
+
+            FTPAccess.TryGetFilelist(path, out files, true);
+            return files;
+        }
+
+        private long GetFileSizes(List<string> fileList)
+        {
+            long sizeOverAll = 0;
+            try
+            {
+                foreach (String file in fileList)
+                {
+
+                    try
+                    {
+                        FtpWebRequest reqSize = (FtpWebRequest)FtpWebRequest.Create(this.Uri + file);
+                        reqSize.ConnectionGroupName = "Test";
+                        reqSize.Credentials = this.NetworkCredential;
+                        reqSize.Method = WebRequestMethods.Ftp.GetFileSize;
+                        reqSize.UseBinary = true;
+                        FtpWebResponse respSize = (FtpWebResponse)reqSize.GetResponse();
+                        long size = respSize.ContentLength;
+                        respSize.Close();
+
+                        PrintText(String.Format("{0,10} {1,-30}", size, file));
+
+                        sizeOverAll += size;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex is WebException && file.Contains("FTPD_PIP."))
+                        { 
+                            // this is a temporary file of the FTP Daemon
+                            // do nothing
+                        }
+                        else
+                        { 
+                            PrintText(String.Format("FAILURE could not get file size:", file));
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // wenn er hier vorbeikommt, dann ist das Verzeichnis leer
+                MessageBox.Show(ex.Message + (ex.InnerException == null ? "" : ("\n" + ex.InnerException.InnerException)));
+            }
+
+            return sizeOverAll;
+        }
+    }
+}
