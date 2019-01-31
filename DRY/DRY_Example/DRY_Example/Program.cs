503f02f582e3d4099a804409fc465c3c085e9540
Siegfried Maier <siegfried.maier@cohu.com>
2019-01-31 10:53:55 +0100

    updated DRY sample and deleted not needed ConsoleApplication

--- DRY/DRY_Example/DRY_Example/Program.cs
+++ DRY/DRY_Example/DRY_Example/Program.cs
@@ -10,25 +10,24 @@ namespace DRY_Example
     {
         static void Main(string[] args)
         {
+            #region CopiedCode
             Console.WriteLine("Michael ist Arzt, wohnt in Rosenheim ud ist 34 Jahre alt");
             Console.WriteLine("Sandra ist Anwältin, wohnt in Hamburg ud ist 39 Jahre alt");
             Console.WriteLine("Stefan ist Elektriker, wohnt in Berlin ud ist 22 Jahre alt");
             Console.WriteLine("Maria ist Bankkauffrau, wohnt in München ud ist 19 Jahre alt");
+            #endregion
 
-
-
-
-
-
+            #region DRYCode
             WritePersonDataToScreen("Michael", "Arzt", "Rosenheim", 34);
             WritePersonDataToScreen("Sandra", "Anwältin", "Hamburg", 39);
             WritePersonDataToScreen("Stefan", "Elektriker", "Berlin", 22);
             WritePersonDataToScreen("Maria", "Bankkauffrau", "München", 19);
+            #endregion
         }
 
         public static void WritePersonDataToScreen(string name, string job, string placeOfResidence, int age)
         {
-            Console.WriteLine("{0} ist {1}, lebt in {2} und ist {3} Jahre alt",
+            Console.WriteLine($"{name} ist {job}, lebt in {placeOfResidence} und ist {age} Jahre alt",
                 name, job, placeOfResidence, age);
         }
     }
