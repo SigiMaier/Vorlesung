59a429b4e6ba4bc5df979a524e6a727e64b7949a
SigiMaier <mail.siegfried.maier@gmail.com>
2019-03-25 22:42:18 +0100

    Update EmployeeFacade.cs

--- 4_SOLID/SOLID_Samples/SRP/SRP_CleanUp2/EmployeeFacade.cs
+++ 4_SOLID/SOLID_Samples/SRP/SRP_CleanUp2/EmployeeFacade.cs
@@ -1,31 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SRP.SRP_CleanUp2
+﻿namespace SRP.SRP_CleanUp2
 {
     public class EmployeeFacade
     {
-        private readonly EmployeeData employee = new EmployeeData();
-        private readonly PayCalculator payCalculator = new PayCalculator();
-        private readonly HourReporter hourReporter = new HourReporter();
-        private readonly EmployeeSaver employeeSaver = new EmployeeSaver();
+        private EmployeeData employee = new EmployeeData();
+        private PayCalculator payCalculator = new PayCalculator();
+        private HourReporter hourReporter = new HourReporter();
+        private EmployeeSaver employeeSaver = new EmployeeSaver();
 
         public void CalculatePay()
         {
-            this.employee.Pay = payCalculator.CalculatePay();
+            this.employee.Pay = this.payCalculator.CalculatePay();
         }
 
         public void ReportHours()
         {
-            this.employee.HoursReport = hourReporter.ReportHours();
+            this.employee.HoursReport = this.hourReporter.ReportHours();
         }
 
         public void Save()
         {
-            employeeSaver.SaveEmployee(this.employee);
+            this.employeeSaver.SaveEmployee(this.employee);
         }
     }
 }
